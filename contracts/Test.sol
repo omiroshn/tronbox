764afb90c0bbec4c3fa738168c19a0eb98752750
omiroshn <omiroshn@student.unit.ua>
2019-04-17 17:53:46 +0300

    initial

---
+++ contracts/Test.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.4.23;
+contract Test{
+    function f() public pure returns (string){
+        return "method f()";
+    }
+    function g() public pure returns (string){
+        return "method g()";
+    }
+}
